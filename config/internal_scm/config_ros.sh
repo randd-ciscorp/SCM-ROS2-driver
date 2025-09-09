fc7d06df97e6d480101d36fb546ebeaf1e7333f9
leoboule <leoboule@ciscorp.co.jp>
2025-09-09 10:02:03 +0900

    Rose max message size

--- config/internal_scm/config_ros.sh
+++ config/internal_scm/config_ros.sh
@@ -1,10 +1,7 @@
 source /opt/ros/humble/setup.bash
-source /root/ros/install/setup.bash
 
 #export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=file:///root/ros/cyclone_config.xml
 
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=/root/ros/fastdds_config.xml
-
-export ROS_DOMAIN_ID=5
