b83bcb8e412161679ce1ee50d57d7be261b1a752
leoboule <leoboule@ciscorp.co.jp>
2025-09-22 19:25:49 +0900

    Add: auto rviz config (rgb)

--- launch/rgb_viz_launch.py
+++ launch/rgb_viz_launch.py
@@ -6,16 +6,22 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    shrd_pkg_prefix = get_package_share_directory("cis_scm")
+
     return LaunchDescription(
         [
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    get_package_share_directory("cis_scm")
-                    + "/launch"
-                    + "/rgb_launch.py"
+                    shrd_pkg_prefix + "/launch" + "/rgb_launch.py"
                 )
             ),
-            Node(package="rviz2", executable="rviz2", name="rviz2", output="screen"),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                output="screen",
+                arguments=["-d", shrd_pkg_prefix + "/config/rviz/scm_rgb.rviz"],
+            ),
             Node(
                 package="tf2_ros",
                 executable="static_transform_publisher",
