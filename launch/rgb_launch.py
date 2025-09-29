5ac684c866a0cc7c483b11556b9b6eecaff41959
leoboule <leoboule@ciscorp.co.jp>
2025-09-29 11:44:47 +0900

    Update launch python code to flake8 compliance

--- launch/rgb_launch.py
+++ launch/rgb_launch.py
@@ -1,32 +1,56 @@
 import os
+
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python import get_package_share_directory
+
 
 def generate_launch_description():
-
     config = os.path.join(
         get_package_share_directory('cis_scm'), 'config', 'params.yaml'
     )
 
-    return LaunchDescription([
-        Node(
-            package="cis_scm",
-            namespace="cis_scm",
-            executable="rgb_node",
-            name="rgb_node",
-            parameters=[{"camera_params": "package://cis_scm/cam_param.yaml"}, config]
-        ),
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            name="camera_base_to_camera",
-            arguments=['--z', '0.03', '--frame-id', 'camera_base', '--child-frame-id' ,'camera']
-        ),
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            name="camera_to_cam_depth",
-            arguments=['--x', '0.001', '--yaw', '-1.57', '--roll', '-1.57', '--frame-id', 'camera', '--child-frame-id', 'cam_depth']
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package='cis_scm',
+                namespace='cis_scm',
+                executable='rgb_node',
+                name='rgb_node',
+                parameters=[
+                    {'camera_params': 'package://cis_scm/cam_param.yaml'},
+                    config,
+                ],
+            ),
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name='camera_base_to_camera',
+                arguments=[
+                    '--z',
+                    '0.03',
+                    '--frame-id',
+                    'camera_base',
+                    '--child-frame-id',
+                    'camera',
+                ],
+            ),
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name='camera_to_cam_depth',
+                arguments=[
+                    '--x',
+                    '0.001',
+                    '--yaw',
+                    '-1.57',
+                    '--roll',
+                    '-1.57',
+                    '--frame-id',
+                    'camera',
+                    '--child-frame-id',
+                    'cam_depth',
+                ],
+            ),
+        ]
+    )
--- launch/rgb_viz_launch.py
+++ launch/rgb_viz_launch.py
@@ -1,38 +1,38 @@
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    shrd_pkg_prefix = get_package_share_directory("cis_scm")
+    shrd_pkg_prefix = get_package_share_directory('cis_scm')
 
     return LaunchDescription(
         [
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    shrd_pkg_prefix + "/launch" + "/rgb_launch.py"
+                    shrd_pkg_prefix + '/launch' + '/rgb_launch.py'
                 )
             ),
             Node(
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
-                output="screen",
-                arguments=["-d", shrd_pkg_prefix + "/config/rviz/scm_rgb.rviz"],
+                package='rviz2',
+                executable='rviz2',
+                name='rviz2',
+                output='screen',
+                arguments=['-d', shrd_pkg_prefix + '/config/rviz/scm_rgb.rviz'],
             ),
             Node(
-                package="tf2_ros",
-                executable="static_transform_publisher",
-                name="map_to_camera_base",
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name='map_to_camera_base',
                 arguments=[
-                    "--z",
-                    "1.0",
-                    "--frame-id",
-                    "map",
-                    "--child-frame-id",
-                    "camera_base",
+                    '--z',
+                    '1.0',
+                    '--frame-id',
+                    'map',
+                    '--child-frame-id',
+                    'camera_base',
                 ],
             ),
         ]
